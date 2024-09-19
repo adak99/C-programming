581be25b800a910dffc6ba17dfd1e486a9f28cd3
Soumya ! <soumyadipadak99@gmail.com>
2024-09-19 20:33:44 +0530

    update

--- Assignments/Assignment 3/question_1.c
+++ Assignments/Assignment 3/question_1.c
@@ -1,107 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct stack
-{
-    int size;
-    int *arr;
-    int top;
+struct stack {
+  int size;
+  int *arr;
+  int top;
 };
 
-void push(struct stack *ptr, int value)
-{
-    if (ptr->top == ptr->size - 1)
-    {
-        printf("Stack overflow: %d can't be pushed.\n", value);
-    }
-    else
-    {
-        ptr->top++;
-        ptr->arr[ptr->top] = value;
-        printf("Inserted %d into the stack.\n", value);
-    }
+void push(struct stack *ptr, int value) {
+  if (ptr->top == ptr->size - 1) {
+    printf("Stack overflow: %d can't be pushed.\n", value);
+  } else {
+    ptr->top++;
+    ptr->arr[ptr->top] = value;
+    printf("Inserted %d into the stack.\n", value);
+  }
 }
 
-int pop(struct stack *ptr)
-{
-    if (ptr->top == -1)
-    {
-        printf("Stack underflow.\n");
-        return -1;
-    }
-    else
-    {
-        int val = ptr->arr[ptr->top];
-        ptr->top--;
-        return val;
-    }
+int pop(struct stack *ptr) {
+  if (ptr->top == -1) {
+    printf("Stack underflow.\n");
+    return -1;
+  } else {
+    int val = ptr->arr[ptr->top];
+    ptr->top--;
+    return val;
+  }
 }
 
-void getData(struct stack *ptr)
-{
-    if (ptr->top == -1)
-    {
-        printf("Stack is empty.\n");
+void getData(struct stack *ptr) {
+  if (ptr->top == -1) {
+    printf("Stack is empty.\n");
+  } else {
+    printf("Stack elements: \n");
+    for (int i = ptr->top; i >= 0; i--) {
+      printf("%d\n", ptr->arr[i]);
     }
-    else
-    {
-        printf("Stack elements: \n");
-        for (int i = ptr->top; i >= 0; i--)
-        {
-            printf("%d\n", ptr->arr[i]);
+  }
+}
+
+int main() {
+  struct stack *s = (struct stack *)malloc(sizeof(struct stack));
+  printf("Enter the stack size: ");
+  scanf("%d", &s->size);
+  s->top = -1;
+  s->arr = (int *)malloc(s->size * sizeof(int));
+
+  int choice, value;
+
+  while (1) {
+    printf("Menu:\n");
+    printf("1. Push.\n");
+    printf("2. Pop.\n");
+    printf("3. Display.\n");
+    printf("4. Exit.\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+      case 1:
+        printf("Enter your data: ");
+        scanf("%d", &value);
+        push(s, value);
+        break;
+      case 2:
+        value = pop(s);
+        if (value != -1) {
+          printf("Popped value: %d\n", value);
         }
+        break;
+      case 3:
+        getData(s);
+        break;
+      case 4:
+        printf("Exiting program.\n");
+        free(s->arr);
+        free(s);
+        return 0;
+      default:
+        printf("Enter a valid choice.\n");
+        break;
     }
-}
-
-int main()
-{
-    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
-    printf("Enter the stack size: ");
-    scanf("%d", &s->size);
-    s->top = -1;
-    s->arr = (int *)malloc(s->size * sizeof(int));
-
-    int choice, value;
-
-    while (1)
-    {
-        printf("Menu:\n");
-        printf("1. Push.\n");
-        printf("2. Pop.\n");
-        printf("3. Display.\n");
-        printf("4. Exit.\n");
-
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice)
-        {
-        case 1:
-            printf("Enter your data: ");
-            scanf("%d", &value);
-            push(s, value);
-            break;
-        case 2:
-            value = pop(s);
-            if (value != -1)
-            {
-                printf("Popped value: %d\n", value);
-            }
-            break;
-        case 3:
-            getData(s);
-            break;
-        case 4:
-            printf("Exiting program.\n");
-            free(s->arr);
-            free(s);
-            return 0;
-        default:
-            printf("Enter a valid choice.\n");
-            break;
-        }
-    }
-    return 0;
+  }
+  return 0;
 }
 
 /*output
--- Assignments/Assignment 3/question_3.c
+++ Assignments/Assignment 3/question_3.c
@@ -1,46 +1,47 @@
 #include <stdio.h>
 #include <string.h>
 
-struct stack
-{
-    char arr[20];
-    int top;
+struct stack {
+  char arr[20];
+  int top;
 };
 
-void push(struct stack *s, char ch)
-{
-    s->top++;
-    s->arr[s->top] = ch;
+void push(struct stack *s, char ch) {
+  s->top++;
+  s->arr[s->top] = ch;
 }
 
-char pop(struct stack *s)
-{
-    char ch = s->arr[s->top];
-    s->top--;
-    return ch;
+char pop(struct stack *s) {
+  char ch = s->arr[s->top];
+  s->top--;
+  return ch;
 }
 
-void main()
-{
-    struct stack s;
-    s.top = -1;
+void main() {
+  struct stack s;
+  s.top = -1;
 
-    char str[20];
-    char ch;
+  char str[20];
+  char ch;
 
-    printf("Enter a string: ");
-    fgets(str, 20, stdin);
-    str[strcspn(str, "\n")] = '\0';
+  printf("Enter a string: ");
+  fgets(str, 20, stdin);
+  str[strcspn(str, "\n")] = '\0';
 
-    for (int i = 0; i < strlen(str); i++)
-    {
-        push(&s, str[i]);
-    }
+  for (int i = 0; i < strlen(str); i++) {
+    push(&s, str[i]);
+  }
 
-    printf("Reverse String: ");
-    for (int i = 0; i < strlen(str); i++)
-    {
-        ch = pop(&s);
-        printf("%c", ch);
-    }
+  printf("Reverse String: ");
+  for (int i = 0; i < strlen(str); i++) {
+    ch = pop(&s);
+    printf("%c", ch);
+  }
 }
+
+/* output->
+
+Enter a string: Hello
+Reverse String: olleH
+
+*/
