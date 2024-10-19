b075c1f48284cb454e76eb0e8472a4b00da1c1e0
Soumya ! <soumyadipadak99@gmail.com>
2024-10-19 17:55:45 +0530

    Bubble sort

--- Sorting in c/Bubble_sort.c
+++ Sorting in c/Bubble_sort.c
@@ -6,11 +6,11 @@ int main()
     int size = sizeof(arr) / sizeof(arr[0]); // size of the array
 
     // Bubble sort
-    for (int i = 0; i < size; i++) // outer loop
+    for (int i = 0; i < size - 1; i++) // outer loop
     {
         for (int j = 0; j < size - i - 1; j++) // iner loop
         {
-            if (arr[j] < arr[j + 1]) // arr j element is less then arr j + 1 element
+            if (arr[j] > arr[j + 1]) // arr j element is less then arr j + 1 element
             {
                 // swaping
                 int temp = arr[j];
---
+++ Sorting in c/Selection_sort.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {4, 5, 3, 1, 6, 2};          // array
+    int size = sizeof(arr) / sizeof(arr[0]); // size of the array
+
+    for (int i = 0; i < size - 1; i++)
+    {
+    }
+}
