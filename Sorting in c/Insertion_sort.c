6984d7efba894a46b8f5c555e3746e2e7916b305
Soumya ! <soumyadipadak99@gmail.com>
2024-10-19 18:11:59 +0530

    insertion sort

--- Sorting in c/Insertion_sort.c
+++ Sorting in c/Insertion_sort.c
@@ -4,6 +4,23 @@ int main()
 {
     int arr[] = {4, 5, 3, 1, 6, 2};          // array
     int size = sizeof(arr) / sizeof(arr[0]); // size of the array
+
+    for (int i = 1; i < size; i++)
+    {
+        int currentElement = arr[i]; // set a current element
+        int j = i - 1;               // set a j-th pointer to making the spaces
+
+        // if j is 0 or j greeter then 0 and our current element is smaller then arr[j]-th element
+        //  then the loop is running
+        while (j >= 0 && currentElement < arr[j])
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = currentElement;
+    }
+
     // print array
     for (int i = 0; i < size; i++)
     {
